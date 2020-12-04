86520bd3bcf1a8bab6398163e43ad6d750f8c9c6
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-03 23:38:03 -0800

    Created Header Component with app title and descripton of filter and sort functions

---
+++ directory/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>Employee Directory</h1>
+            <p>Select a month to filter employees by their birth month and click the button to sort by last name</p>
+        </div>
+    )
+}
+
+export default Header;
